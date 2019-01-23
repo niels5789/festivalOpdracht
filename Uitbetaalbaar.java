4e36576f18cfa6c07acef78656ae7462e7075615
Hevar Marouf <maroufhevar@gmail.com>
2019-01-23 15:48:25 +0100

    kingisback

---
+++ Uitbetaalbaar.java
@@ -0,0 +1,5 @@
+package festivalProject;
+
+public interface Uitbetaalbaar {
+
+}
--- festivalMain.java
+++ festivalMain.java
@@ -10,25 +10,41 @@ public class festivalMain {
 
 	private void startFestival() {
 
+		
 //		****** opzetten festival		
-//		Maak array voor festivalDagen met random weercijfers (10 dagen)
+//		[1]Maak array voor festivalDagen met random weercijfers (10 dagen)
+//		 - [1]Toon weerbericht voor alle dagen
+//		 - [1]Huur medewerkes in (1 medewerker bediend 10 gasten)
+//	     - [3]Budget aanmaken voor Acts
 		
 //		Maak array acts (kosten, omzet)
-		
+//		Koop Acts in
+//		 - [1]Kosten per act (afhankelijk van soort act)
+//		 - [1]Geef AantrekkelijkheidsCijfer mee (afhankelijk van weer)
+		 
 //		Maak festival gangers array ()
-		
-		
-		
+//		 - [1]Aantal Afhankelijk van weer
+//		 - [2]Afhankelijk van Acts
 		
 		
 //		******* Loop over dagen *******
 		
 //		start festival dag
-//			- loop over gasten. Afhankelijk van het weer bepalen we welke act (Beter weer is duurdere act) 
-//			- ga feesten
+//			- [1]Loop over gasten.
+//			- [2]Afhankelijk van het weer bepalen we welke act (Beter weer is duurdere act) 
+//			- [1]1 act per dag per festival ganger
+//				* heeft nog genoeg geld?
+//				* Zijn er genoeg medewerkers?
+//				* Welke act is er gekozen?
+//			- [2]Voeg omzet toe 
 		
 //		******* einde loop *******
-		
+////			- [3]Toon financieel rapport
+//				* Totale Uitgave
+//				* Totale Opbrengst
+//				* Winst
+				
+//				- [3]Stap 2: Uitsplitsen per Act
 	}
 
 }
